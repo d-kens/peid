c293f552c3c6c60c55e36620c88a5e3c97147648
d-kens <omondikens1@gmail.com>
2026-01-06 22:09:52 +0300

    Login page html markup and styles

--- src/app/features/auth/components/login/login.ts
+++ src/app/features/auth/components/login/login.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {Component, signal} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -27,32 +27,11 @@ import {CommonModule} from '@angular/common';
   styleUrl: './login.css',
 })
 export class Login {
-  readonly loading = signal(false);
-  readonly hidePassword = signal(true);
-  readonly form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      rememberMe: [true],
-    });
-  }
+  hidePassword = signal(true)
 
-  get email() { return this.form.get('email'); }
-  get password() { return this.form.get('password'); }
-
-  togglePasswordVisibility() {
-    this.hidePassword.update(v => !v);
-  }
-
-  submit() {
-    if (this.form.invalid || this.loading()) return;
-    this.loading.set(true);
-    // Simulate login call
-    const payload = this.form.value;
-    console.log('Login submit', payload);
-    // In a real app, inject a service and navigate on success.
-    setTimeout(() => this.loading.set(false), 800);
+  togglePasswordVisibility(event: MouseEvent) {
+    this.hidePassword.set(!this.hidePassword())
+    event.stopPropagation();
   }
 }
